d33b20933660a793da62695c7a3ae1bcb84cdad6
makepost <makepost@firemail.cc>
2018-10-28 22:45:59 +0200

    Prepare: Remove transpilation source map
    TypeScript output is readable, and the map doesn't fully work unless I package the source file along or embed it in the map.

--- prepare.js
+++ prepare.js
@@ -5,14 +5,11 @@ const { join } = require("path");
 // @ts-ignore
 const _ = __dirname;
 const { ScriptTarget, transpileModule } = require("typescript");
-const { outputText, sourceMapText } = transpileModule(
+const { outputText } = transpileModule(
   readFileSync(join(_, "index.js"), "utf8"),
-  {
-    compilerOptions: { sourceMap: true, target: ScriptTarget.ES5 }
-  }
+  { compilerOptions: { target: ScriptTarget.ES5 } }
 );
 writeFileSync(join(_, "index.dist.js"), outputText);
-writeFileSync(join(_, "index.dist.js.map"), sourceMapText);
 writeFileSync(join(_, "index.dist.d.ts"), readFileSync(join(_, "index.d.ts")));
 writeFileSync(
   join(_, "index.dist.test.js"),
